1337b510eeb63b159cba8032cf50039f3c768ada
emily <focusonemily@gmail.com>
2024-03-27 00:37:02 +0900

    #7 feat:colors.css global color수정 및 SelectedFile적용

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,8 +6,9 @@ function App() {
       <div className="App">
         <div>nav</div>
         <Routes>
-          <Route path="main" element={<div><SelectedFile name={"이건파일명"}>"눌러"</SelectedFile>
-          <input></input></div>} />
+          <Route path="main" element={<div>main</div>} />
+          {/*컴포넌트 테스트를 위한 임시 path */}
+          <Route path="zzuyeontest" element={<div><SelectedFile name={"이건파일명"}>X</SelectedFile></div>} />
         </Routes>
         <div>footer</div>
       </div>
