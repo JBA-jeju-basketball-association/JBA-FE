39a62ede9a069a75256579bc3a84bcc1e5833d7c
emily <focusonemily@gmail.com>
2024-03-28 21:28:18 +0900

    #11 회원가입 폼 Input컴포넌트 및 정규표현식 개발

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
+import { SignupForm } from "./signup/ui/SignupForm";
 function App() {
   return (
       <div className="App">
         <div>nav</div>
         <Routes>
-          <Route path="main" element={<div>메인</div>} />
+          <Route path="main" element={<div><SignupForm/></div>} />
         </Routes>
         <div>footer</div>
       </div>
