04c84cc4bf9c3f2ddf0eba6065e50afad80b1e26
emily <focusonemily@gmail.com>
2024-04-02 00:37:40 +0900

    #11 feat:성별 입력창 구현 및 회원가입폼 데이터 수정 함수 생성및 적용

--- src/app/signup/ui/SignupForm.tsx
+++ src/app/signup/ui/SignupForm.tsx
@@ -1,56 +1,58 @@
-import React, { useEffect, useState } from "react" 
+import React from "react" 
 import SignupInput from "./SignupInput/SignupInput";
 import styles from"./SignupFrom.module.css"
 
+  // type은 따로 관리예정입니다.
 
-//타입 임시 작성
-// interface SignupForm {
-//     email: string;
-//     password: string;
-//     passwordConfirm: string;
-//     name: string;
-//     phonenumber: string;
-//     team: string;
-//     birthday: string;
-//     gender: "MALE"|"FEMALE";
-//   }
+
+interface SignupFormData {
+    email: string;
+    password: string;
+    passwordConfirm: string;
+    name: string;
+    phoneNum: string;
+    team: string;
+    birthday: string;
+    gender: "MALE"|"FEMALE"|"";
+  }
   
-
+type Key ="email"|"password"|"passwordConfirm"|"name"|"phoneNum"|"team"|"birthday"|"gender"
 
 
 
 export function SignupForm(){
-    const [signupForm,setSignupForm]=useState({
+    const signupFormData:SignupFormData={
         email: "",
         password: "",
         passwordConfirm: "",
         name: "",
-        phonenumber: "",
+        phoneNum: "",
         team:"",
         birthday: "",
-        gender: "",
-    });
+        gender: ""};
 
-    function changeSingupForm(type: string, value: string) {
-      setSignupForm(prev => ({
-        ...prev,
-        [type]: value 
-      }));
+  
+
+    function changeSingupForm(type: Key, value: string | "MALE" | "FEMALE" | "") {
+      // gender인 경우 별도로 처리
+      if (type === "gender") {
+          signupFormData["gender"] = value as "MALE" | "FEMALE" | "";
+      } else {
+          signupFormData[type] = value;
+      }
     }
-    useEffect(()=>{
-      console.log(signupForm)
-    },[signupForm])
+
     return(
     <div className={styles.SignupForm}>
             <SignupInput type={"email"} changeSingupForm={changeSingupForm} placeholder={"이메일"}></SignupInput>
             <SignupInput type={"password"} changeSingupForm={changeSingupForm} placeholder={"비밀번호"}></SignupInput>
             <SignupInput type={"name"} changeSingupForm={changeSingupForm} placeholder={"이름"}></SignupInput>
-            <SignupInput type={"phoneNum"} changeSingupForm={changeSingupForm} placeholder={"전화번호"}></SignupInput>
+            <SignupInput type={"phoneNum"} changeSingupForm={changeSingupForm} placeholder={"000-0000-0000"}></SignupInput>
             <SignupInput type={"team"} changeSingupForm={changeSingupForm} placeholder={"소속팀"}></SignupInput>
+            <SignupInput type={"gender"} changeSingupForm={changeSingupForm} placeholder={"소속팀"}></SignupInput>
+            <button onClick={()=>console.log(signupFormData)}> 확인 테스트</button>
+
     </div>
     );
 };
 
-
-//성별과 비밀번호 확인 input 컴포넌트 별도 구현
-//팀 무소속시 무소속관련 토글버튼 구현
--- src/app/signup/ui/SignupInput/SignupInput.tsx
+++ src/app/signup/ui/SignupInput/SignupInput.tsx
@@ -1,49 +1,69 @@
-import React, { useState } from "react" 
+import React, { useState, useEffect } from "react" 
 import checkloginInPutData from"../../checkloginInPutData"
 import style from"./SignupInput.module.css"
 
+type Key ="email"|"password"|"passwordConfirm"|"name"|"phoneNum"|"team"|"birthday"|"gender"
 
 
 type Props={
     icon?:string;
-    type:string;
+    type:Key;
     placeholder?:string;
-    changeSingupForm:(type: string, value: string)=>void;
+    changeSingupForm:(type: Key, value: string | "MALE" | "FEMALE" | "")=>void;
 }
 
 
 
 export default function SignupInput({icon,type,placeholder,changeSingupForm}:Props){
+  const loginInputData: [RegExp, string, string] = checkloginInPutData(type);
+  const regex: RegExp = loginInputData[0];
+  const isTrue: string = loginInputData[1];
+  const isFalse: string = loginInputData[2];   
+  const[inputData,setInputData]=useState("")
+  const[toggle,setToggle]=useState(false)
+  const[guideline,setGuideline]=useState(false)
+  
 
-    const [regex,isTrue,IsFalse]:[RegExp,string,string]=checkloginInPutData(type)
-    const[inputData,setInputData]=useState("")
-    const[guideline,setGuideline]=useState(IsFalse)
+  useEffect(()=>{
+    setInputData((currentValue) => currentValue);
+    if (!regex.test(inputData)){
+      setGuideline(false)
+      changeSingupForm(type,"")
+    }else{ setGuideline(true)
+      changeSingupForm(type,inputData)
+    }},
     
+    [inputData,regex,type,changeSingupForm])
+
+
+
     function handleChange (event: React.ChangeEvent<HTMLInputElement>){
       setInputData(event.target.value)
-      if( !regex.test(inputData)){
-        changeGuideline(IsFalse)
-        changeSingupForm(type,"")
+  
+    }
+
+    function handleClick (event: React.MouseEvent<HTMLInputElement>){
+      setToggle(!toggle);
+      if(inputData===event.currentTarget.value){
+        setInputData("")
       }else{
-        changeGuideline(isTrue)
-        changeSingupForm(type,inputData)
+       setInputData(event.currentTarget.value)
       }
-  
-    }
-  
-    function changeGuideline(e:string){
-      setGuideline(e)
-    }
-    
+      }
+
+
+
+
       return(
           <div className={style.SignupInput}>
-                <div  className={style.SignupInput_input}>
+                <div  className={style.SignupInput_inputBox}>
                     <div  className={style.SignupInput_iconbox}>
                         <img  src={icon} alt="x" />
                     </div>
-                    <input type="string" placeholder={placeholder} onChange={(e)=>{handleChange(e)}}></input>
+                    {type==="gender"?<SelectGender handleClick={handleClick} toggle={toggle} inputData={inputData}></SelectGender>:
+                    <input type={type==="password"? "password":"string"}placeholder={placeholder} onChange={(e)=>{handleChange(e)}}></input>}
                 </div>
-                <div className={style.SignupInput_guideline}>{guideline}</div>
+                <div className={style.SignupInput_guideline}>{guideline?isTrue:isFalse}</div>
           </div>
               );
   }
@@ -53,4 +73,21 @@ export default function SignupInput({icon,type,placeholder,changeSingupForm}:Pro
   
   
   
- 
+  function SelectGender({handleClick,toggle,inputData}:{handleClick:(event: React.MouseEvent<HTMLInputElement>)=>void,toggle:boolean,inputData:string}){
+    return(
+    <div  className={style.SignupInput_input_gender}>
+      <div className={style.SignupInput_input_gender_checkbox}>
+        <input type="checkbox" id="MALE" value="MALE" name="gender" onClick={(e)=>{handleClick(e)
+        } } checked={inputData==="MALE"?true:false} />
+        <label htmlFor="MALE">남자</label>
+        </div>
+        <div className={style.SignupInput_input_gender_checkbox}>
+        <input type="checkbox" id="FEMALE" value="FEMALE" name="gender"  onClick={(e)=>{handleClick(e)}} checked={inputData==="FEMALE"?true:false} />
+        <label htmlFor="FEMALE">여자</label>
+      </div>
+      <button onClick={()=>{console.log()}}></button>
+     </div>
+    )
+  }
+
+  
