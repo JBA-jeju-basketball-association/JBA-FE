27e524c00b602f87439e11e76bae1cad74b553d0
shinhansol <wkdqhrh010@naver.com>
2024-04-03 16:50:12 +0900

    #13 feat: App.tsx에 route 설정 완료

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
 import Test from "./Test";
-function App() {
-  return (
-      <div className="App">
-        <div>nav</div>
-        <Routes>
-          <Route path="main" element={<div>메인</div>} />
-            <Route path="test" element={<Test />} />
-        </Routes>
-        <div>footer</div>
-      </div>
-  );
+import LoginPage from "../pages/loginPage/LoginPage";
+function App():React.JSX.Element {
+    return (
+        <div className="App">
+            <div>nav</div>
+            <Routes>
+                <Route path="main" element={<div>메인</div>}/>
+                <Route path="login" element={<LoginPage />} />
+                <Route path="test" element={<Test/>}/>
+            </Routes>
+            <div>footer</div>
+        </div>
+    );
 }
 //appRouter
 
