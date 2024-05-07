254269ec7f7aa6dd0f1350bff29791e87feb6e30
shinhansol <wkdqhrh010@naver.com>
2024-05-07 19:24:02 +0900

    32 fix: api 요청 url 을 서버 url 로 변경

--- src/shared/api/NormalApi.ts
+++ src/shared/api/NormalApi.ts
@@ -1,11 +1,10 @@
 
 
 import axios, {AxiosInstance} from "axios";
-import * as process from "process";
 
 export const NormalApi:AxiosInstance = axios.create({
-    // baseURL: `${process.env.BACKEND_SERVER_URL}`, // 배포된 서버 사용시
-    baseURL: "http://localhost:8080", // 로컬 서버 사용시
+    baseURL: process.env.REACT_APP_SERVER_URL, // 배포된 서버 사용시
+    // baseURL: "http://localhost:8080", // 로컬 서버 사용시
     headers: {
         Accept:"application/json",
     }
