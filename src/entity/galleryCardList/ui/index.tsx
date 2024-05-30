61088540e76bb688302c122308f6dcb769e4e541
jiyong <parkjiy96@naver.com>
2024-05-30 13:01:59 +0900

    feat : react-query로 갤러리  데이터 패칭

--- src/entity/galleryCardList/ui/index.tsx
+++ src/entity/galleryCardList/ui/index.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
 import GalleryCard from "shared/ui/galleryCard";
-import { Api } from "../../../shared/api/Api";
-import axios from "axios";
-import { useState } from "react";
 import styles from "./galleryCardList.module.css";
 
 type GalleryCardType = {
@@ -12,31 +9,18 @@ type GalleryCardType = {
   imgUrl: string;
 };
 
-const GalleryCardList = () => {
-  const [galleries, setGalleries] = useState<GalleryCardType[]>([]);
-  const fetchGalleryImage = async () => {
-    try {
-      const res = await axios.get(
-        "http://ec2-43-200-4-149.ap-northeast-2.compute.amazonaws.com:8080/v1/api/gallery?page=0&size=6&official=true"
-      );
-      setGalleries(res.data.data.galleries);
-    } catch (error) {
-      console.error("갤러리 이미지를 가져오는 중 오류 발생:", error);
-    }
-  };
-  console.log(galleries);
+type GalleryCardListType = {
+  galleries: GalleryCardType[];
+};
 
-  useEffect(() => {
-    fetchGalleryImage();
-  }, []);
-
+const GalleryCardList = ({ galleries }: any) => {
   return (
     <div className={styles.container}>
-      {galleries.map((card) => (
+      {galleries.map((gallery: GalleryCardType) => (
         <GalleryCard
-          key={card.galleryId}
-          title={card.title}
-          imgUrl={card.imgUrl}
+          key={gallery.galleryId}
+          title={gallery.title}
+          imgUrl={gallery.imgUrl}
         />
       ))}
     </div>
--- src/pages/galleryPage/ui/index.tsx
+++ src/pages/galleryPage/ui/index.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GalleryCardList from "entity/galleryCardList/ui";
 import styles from "./galleryPage.module.css";
+import { Pagination } from "widgets/pagination";
+import { SearchBar } from "widgets/searchBar";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+type GalleryCardType = {
+  galleryId: number;
+  title: string;
+  fileName: string;
+  imgUrl: string;
+};
 
 const GalleryPage = () => {
+  const [page, setPages] = useState(1);
+
+  const { data: galleryData } = useQuery({
+    queryKey: ["galleries", page],
+    queryFn: () =>
+      axios.get(
+        `http://ec2-43-200-4-149.ap-northeast-2.compute.amazonaws.com:8080/v1/api/gallery?page=${page - 1}&size=6&official=true`
+      ),
+  });
+  const galleries: GalleryCardType = galleryData?.data.data.galleries ?? [];
+  const totalPage: number = galleryData?.data.data.totalPages ?? 0;
+
   return (
     <div className={styles.container}>
       <h1>갤러리</h1>
-      <GalleryCardList />
-      <div>페이지네이션</div>
-      <div>검색</div>
+      <GalleryCardList galleries={galleries} />
+      <Pagination totalPages={totalPage} page={page} setPage={setPages} />
+      {/* <SearchBar /> */}
     </div>
   );
 };
