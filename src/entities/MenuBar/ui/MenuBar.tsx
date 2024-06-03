3c2ff5e81a1060e48dcf0e05169e13bd27269657
emily <focusonemily@gmail.com>
2024-06-03 22:36:54 +0900

    fix: 불필요한 코드 삭제 및 css 변경

--- src/entities/MenuBar/ui/MenuBar.tsx
+++ src/entities/MenuBar/ui/MenuBar.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react"
+import React, { useState } from "react"
 import Style from'./MenuBar.module.css'
 import classNames from 'classnames';
 import { Link } from "react-router-dom"
