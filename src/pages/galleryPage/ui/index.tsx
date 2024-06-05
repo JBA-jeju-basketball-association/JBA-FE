92310ba92f6bd0c4510fe1fc0459e9b9400f2920
jiyong <parkjiy96@naver.com>
2024-06-05 17:15:10 +0900

    feat : 검색기능 추가

--- src/pages/galleryPage/ui/index.tsx
+++ src/pages/galleryPage/ui/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GalleryCardList from "entity/galleryCardList/ui";
 import styles from "./galleryPage.module.css";
 import { Pagination } from "widgets/pagination";
@@ -13,28 +13,62 @@ const GalleryPage = () => {
   const [page, setPage] = useState(1);
   const [searchCategory, setSearchCategory] = useState("제목");
   const [searchKeyword, setSearchKeyword] = useState("");
+  const [filteredGalleries, setFilteredGalleries] = useState<GalleryCardType[]>(
+    []
+  );
 
-  const handleSearch = () => {
-    setPage(1);
-    refetch();
-  };
-
+  // 페이지 전체 조회하는 쿼리
   const { data: galleryData, refetch } = useQuery({
-    queryKey: ["galleries", page, searchCategory, searchKeyword],
+    queryKey: ["galleries", page],
     queryFn: () =>
       Api.get(`/v1/api/gallery`, {
         params: {
           page: page - 1,
           size: 6,
           official: true,
-          searchCategory,
-          searchKeyword,
         },
       }),
   });
+
   const galleries: GalleryCardType[] = galleryData?.data.data.galleries ?? [];
   const totalPage: number = galleryData?.data.data.totalPages ?? 0;
 
+  // 검색 결과 필터링 및 페이지 이동
+  useEffect(() => {
+    if (!galleryData) return;
+    const filterGalleries = () => {
+      return galleries.filter((gallery) =>
+        //galleries 데이터를 필터해서
+        searchCategory === "제목" || searchCategory === "전체"
+          ? //제목이나 전체로 검색했을 때 true면 타이틀을 포함하는 갤러리만 반환
+            gallery.title?.includes(searchKeyword) ?? false
+          : true
+      );
+    };
+    setFilteredGalleries(filterGalleries());
+    //검색어가 바뀌면 필터링된 갤러리를 다시 세팅
+  }, [galleryData, searchCategory]);
+
+  // 검색 결과 페이지로 이동하는 함수 호출
+  const findTargetPage = () => {
+    Api.get(`/v1/api/gallery`, {
+      params: {
+        page: 0,
+        size: totalPage * 6,
+        official: true,
+      },
+    }).then((res) => {
+      const allGalleries = res.data.data.galleries;
+      const foundIndex = allGalleries.findIndex((gallery: GalleryCardType) =>
+        gallery.title?.includes(searchKeyword)
+      );
+      const targetPageNumber =
+        foundIndex !== -1 ? Math.floor(foundIndex / 6) + 1 : 1;
+      setPage(targetPageNumber);
+      refetch();
+    });
+  };
+  //refetch를 사용하면 바로바로 바뀜 -> 검색은 한번 눌러야함
   return (
     <div className={styles.container}>
       <div className={styles.GalleryPageHeader}>
@@ -43,13 +77,13 @@ const GalleryPage = () => {
           <RegitUpdateDeleteButton content="등록하기" />
         </Link>
       </div>
-      <GalleryCardList galleries={galleries} />
+      <GalleryCardList galleries={filteredGalleries} />
       <Pagination totalPages={totalPage} page={page} setPage={setPage} />
       <SearchBar
         searchCategory={searchCategory}
         setSearchCategory={setSearchCategory}
         setSearchKeyword={setSearchKeyword}
-        handleSearch={handleSearch}
+        handleSearch={() => findTargetPage()}
       />
     </div>
   );
