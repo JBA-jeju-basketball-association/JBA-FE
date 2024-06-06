8060dc46ceb38de285add8b465ff93cec465bc51
yunsungLEE <chance07236@naver.com>
2024-06-06 14:19:00 +0900

    chore: 머지를 위한 임시 커밋

--- src/pages/competitionPage/api/FetchGetCompetitionList.ts
+++ src/pages/competitionPage/api/FetchGetCompetitionList.ts
@@ -1,7 +1,13 @@
-import axios, {AxiosResponse} from "axios";
+import axios, { AxiosResponse } from "axios";
+import { NormalApi } from "shared/api/NormalApi";
 
-
-
-export default function fetchGetCompetitionList (statusFocused:string, year:string, page:number, pageSize:number):Promise<AxiosResponse<any,any>>{
-    return axios.get(`http://localhost:8080/v1/api/competition/competition?status=${statusFocused}&year=${year}&page=${page-1}&size=${pageSize}`)
+export default function fetchGetCompetitionList(
+  statusFocused: string,
+  year: string,
+  page: number,
+  pageSize: number
+): Promise<AxiosResponse<any, any>> {
+  return NormalApi.get(
+    `v1/api/competition/competition?status=${statusFocused}&year=${year}&page=${page - 1}&size=${pageSize}`
+  );
 }
