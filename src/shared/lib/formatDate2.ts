e9c36337adcb31790923200a3145ad8be42e7e56
shinhansol <wkdqhrh010@naver.com>
2024-06-09 17:22:37 +0900

    #66 feat: main page 에 competition 부분 구현 및 react-slick 을 이용하여 carousel 구현완료 / api 연동 완료

---
+++ src/shared/lib/formatDate2.ts
@@ -0,0 +1,7 @@
+export default function formatDate2(date:Date) {
+    // 예시 출력: 2024-05-01
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
---
+++ src/widgets/main/ui/CompetitionArea.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import style from "./CompetitionArea.module.css"
+import {MainTitle, NavigateAllItemBtn} from "../../../shared/ui";
+import {CompetitionCarousel} from "./CompetitionCarousel";
+
+export const CompetitionArea = () => {
+
+    return (
+        <div className={style.CompetitionArea}>
+            <MainTitle title={"대회 일정"} color={"black"} />
+            <div className={style.titleBar} ></div>
+            <div className={style.navigateBtnArea}>
+                <NavigateAllItemBtn path={"/competition"} color={"#D4C39C"} />
+            </div>
+            <CompetitionCarousel />
+        </div>
+
+    );
+};
