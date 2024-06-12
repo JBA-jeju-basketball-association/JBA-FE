8893687c5c6e0af8e9f086a42daad32b84f29d78
shinhansol <wkdqhrh010@naver.com>
2024-06-12 09:01:50 +0900

    #71 fix: alert -> sweetAlert 로 변경

--- src/pages/loginPage/api/FetchLogin.ts
+++ src/pages/loginPage/api/FetchLogin.ts
@@ -1,5 +1,6 @@
 import {Api} from "../../../shared/api";
 import React from "react";
+import confirmAlert from "../../../shared/lib/ConfirmAlert";
 
 export default function fetchLogin (email:string, password:string,
                                     setEmailMessage: React.Dispatch<React.SetStateAction<string>>,
@@ -29,9 +30,9 @@ export default function fetchLogin (email:string, password:string,
         else if (data.detailMessage === "Not Found User") setEmailMessage("이메일을 확인해주세요.");
 
         //틀린 비밀번호 입력 시 alert
-        else if (data.detailMessage === "자격 증명에 실패하였습니다.") alert(`비밀번호를 ${data.request.failureCount}회 틀렸습니다. (5회 실패시 계정 잠금)`)
-        else if (data.detailMessage === "자격 증명에 실패하였습니다. 계정이 잠깁니다.") alert("비밀번호를 5회 잘못 입력하셨습니다. 계정이 잠깁니다.")
-        else if (data.detailMessage === "Login Locked User") alert("잠긴 계정입니다.")
-        else if (data.code === 406) alert("로그인 할 수 없습니다. 관리자에게 문의하세요.")
+        else if (data.detailMessage === "자격 증명에 실패하였습니다.") confirmAlert("warning", `비밀번호를 ${data.request.failureCount}회 틀렸습니다.<br>(5회 실패시 계정 잠금)`)
+        else if (data.detailMessage === "자격 증명에 실패하였습니다. 계정이 잠깁니다.") confirmAlert("warning", "비밀번호를 5회 잘못 입력하셨습니다.<br>계정이 잠깁니다.")
+        else if (data.detailMessage === "Login Locked User") confirmAlert("warning", "잠긴 계정입니다.")
+        else if (data.code === 406) confirmAlert("warning", "로그인 할 수 없습니다.<br>관리자에게 문의하세요.")
     });
 }
