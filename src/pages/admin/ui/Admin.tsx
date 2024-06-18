9869166eff09a8d27b094b1c5f01abec695998b4
shinhansol <wkdqhrh010@naver.com>
2024-06-18 17:27:26 +0900

    #90 feat:adminPage Layer 생성

---
+++ src/pages/admin/ui/Admin.tsx
@@ -0,0 +1,107 @@
+import {Button, Layout, Menu, theme} from 'antd';
+import React, {useState} from 'react';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import style from "./Admin.module.css"
+import {JwtDecoder} from "../../../shared/lib";
+import {useUserStore} from "../../../shared/model";
+import {Route, Routes, useNavigate} from "react-router-dom";
+import {AdminUser} from "./AdminUser";
+import {AdminCompetition} from "./AdminCompetition";
+import {AdminPost} from "./AdminPost";
+import {AdminGallery} from "./AdminGallery";
+const { Header, Sider, Content } = Layout;
+
+export const Admin = () => {
+    const adminMenu:string[] = ["user", "competition", "post", "gallery", ]
+    const [collapsed, setCollapsed] = useState(false);
+    const [selectedMenu, setSelectedMenu] = useState<string>(adminMenu[0]);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+    const {AccessToken} = useUserStore();
+
+
+
+    return (
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className={style.logo} ><p>JBA-Admin</p></div>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: '회원 관리',
+                            onClick: () => setSelectedMenu(adminMenu[0])
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: '대회 관리',
+                            onClick: () => setSelectedMenu(adminMenu[1])
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: '게시물 관리',
+                            onClick: () => setSelectedMenu(adminMenu[2])
+                        },
+                        {
+                            key: '4',
+                            icon: <UploadOutlined />,
+                            label: '갤러리 관리',
+                            onClick: () => setSelectedMenu(adminMenu[3])
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{padding: 0, background: colorBgContainer, display: "flex", justifyContent:"space-between"}}>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                    <div className={style.headerRight}>
+                        {AccessToken && <div className={style.emailArea}><p>{JwtDecoder(AccessToken).sub.split("@")[0]}</p></div>}
+                        <button
+                            className={style.backHomeBtn}
+                            onClick={() => window.location.href = "/"}
+                        >돌아가기
+                        </button>
+                    </div>
+                </Header>
+
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    {selectedMenu === adminMenu[0] ? <AdminUser /> :
+                        selectedMenu === adminMenu[1] ? <AdminCompetition /> :
+                            selectedMenu === adminMenu[2] ? <AdminPost /> :
+                                selectedMenu === adminMenu[3] ? <AdminGallery /> : null
+                    }
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
---
+++ src/pages/admin/ui/AdminPost.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AdminPost = () => {
+    return (
+        <div>
+            AdminPost
+        </div>
+    );
+};
