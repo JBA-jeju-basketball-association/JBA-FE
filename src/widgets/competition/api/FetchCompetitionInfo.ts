d9da6f99ff5fffcd8af44c763a49160bc117bfb7
shinhansol <wkdqhrh010@naver.com>
2024-06-21 17:06:19 +0900

    #78 fix: competitionDetail 페이지에서 competitionStatus=NORMAL 이 아닐경우 '/competition' 으로 이동하도록 변경

--- src/widgets/competition/api/FetchCompetitionInfo.ts
+++ src/widgets/competition/api/FetchCompetitionInfo.ts
@@ -1,8 +1,17 @@
 import {NormalApi} from "../../../shared/api";
+import confirmAlert from "../../../shared/lib/ConfirmAlert";
+import {useNavigate} from "react-router-dom";
 
 
 export default function fetchCompetitionInfo(id:string|undefined) {
     if (id) {
-        return NormalApi.get(`/v1/api/competition/detail/${id}`);
+        return NormalApi.get(`/v1/api/competition/detail/${id}`)
+            .catch(err => {
+                if (err.response.data.detailMessage === "해당 아이디와 일치하는 대회를 찾을 수 없습니다." || err.response.data.detailMessage === "대회 조회가 불가능합니다.")
+                    confirmAlert("warning", "대회를 찾을 수 없습니다.")
+                        .then(res => {
+                            if (res.isConfirmed) window.location.href = "/competition"
+                        })
+            })
     }
 }
